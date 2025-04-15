78e6b423cd42c1717856610d1eab4b07c1e1d7bc
GaabrielFerreira <ga.ferreiraqueiroz@hotmail.com>
2025-04-15 19:30:43 -0300

    feat: task struct creation

--- lista_dinamica.h
+++ lista_dinamica.h
@@ -2,9 +2,15 @@
  * Esqueleto para a implementação de listas.
  */
 
-typedef int elem_t;
+typedef struct tarefa{
+   int id;
+   char nome[30];
+   char desc[100];
+   int prioridade;
+   char categoria[20];
+} tarefa;
 typedef struct no{
-  elem_t info;
+  tarefa info;
   struct no *prox;
 } No_lista;
 
@@ -17,15 +23,15 @@ void cria(Lista *p_l);
 int vazia(Lista *p_l);
 
 /* Insere um elemento no início da lista */
-void insere_inicio(Lista *p_l, elem_t e);
+void insere_inicio(Lista *p_l, tarefa e);
 
 /* Insere um elemento no final da lista */
-void insere_fim(Lista *p_l, elem_t e);
+void insere_fim(Lista *p_l, tarefa e);
 
 /* Insere um elemento na lista de maneira ordenada.
    Retorna 0 caso o elemento já exista na lista. 
    Assume que a lista está ordenada */
-int insere_ordenado(Lista *p_l, elem_t e);
+int insere_ordenado(Lista *p_l, tarefa e);
 
 /* Verifica se a lista está ordenada */
 int ordenada(Lista *p_l);
@@ -35,15 +41,15 @@ void ordena(Lista *p_l);
 
 /* Remove o elemento que está no início da lista.
    Retorna 0 caso a lista esteja vazia */
-int remove_inicio(Lista *p_l, elem_t *p_e);
+int remove_inicio(Lista *p_l, tarefa *p_e);
 
 /* Remove o elemento que está no final da lista.
    Retorna 0 caso a lista esteja vazia */
-int remove_fim(Lista *p_l, elem_t *p_e);
+int remove_fim(Lista *p_l, tarefa *p_e);
 
 /* Remove o nó de valor e.
    Retorna 0 caso este nó não tenha sido encontrado */
-int remove_valor(Lista *p_l, elem_t e);
+int remove_valor(Lista *p_l, tarefa e);
 
 /* Inverte os elementos de uma lista */
 void inverte(Lista *p_l);
